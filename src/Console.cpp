c47347625926979ad1eb0f412b95bcdd71f4afee
Zax Ftw <zax-ftw@users.noreply.github.com>
2024-06-25 22:27:59 +0200

    Add `GetXPForNextLevel` command

--- src/Console.cpp
+++ src/Console.cpp
@@ -21,13 +21,30 @@ bool ObScript::AdvancePCLevel_Execute(const ScriptParam* paramInfo, ScriptData* 
 	return true;
 }
 
+bool ObScript::GetXPForNextLevel_Execute(const ScriptParam* paramInfo, ScriptData* scriptData, RE::TESObjectREFR* thisObj, RE::TESObjectREFR* containingObj, RE::Script* scriptObj, RE::ScriptLocals* locals, double& result, std::uint32_t& opcodeOffsetPtr)
+{
+	float points = 0.0f;
+
+	auto player = PlayerCharacter::GetSingleton();
+	if (auto skills = player->GetInfoRuntimeData().skills) {
+		if (auto data = skills->data) {
+			points = data->levelThreshold - data->xp;
+		}
+	}
+
+	auto console = ConsoleLog::GetSingleton();
+	console->Print("GetXPForNextLevel >> %0.2f", points);
+
+	return true;
+}
+
 void ObScript::AdvancePCLevel_Commit(Command* original)
 {
 	Command cmd = *original;
 
 //	cmd.longName = "";
 //	cmd.shortName = "";
-	cmd.helpString = "Give the player the given amount of experience";
+	cmd.helpString = "Give the player the given amount of character experience";
 	cmd.referenceFunction = false;
 	cmd.numParams = 1;
 	cmd.executeFunction = AdvancePCLevel_Execute;
@@ -43,10 +60,37 @@ void ObScript::AdvancePCLevel_Commit(Command* original)
 	REL::safe_write((uintptr_t)original, &cmd, sizeof(cmd));
 }
 
+void ObScript::GetXPForNextLevel_Commit(Command* original) 
+{
+	Command cmd = *original;
+
+	//	cmd.functionName = "";
+	//	cmd.shortName = "";
+	cmd.helpString = "Returns the amount of XP the player needs before reaching the next level";
+	cmd.referenceFunction = false;
+	cmd.numParams = 0;
+	cmd.executeFunction = GetXPForNextLevel_Execute;
+	cmd.editorFilter = false;
+
+	//ScriptParam* param = new ScriptParam();
+	//param->paramName = "Integer";
+	//param->paramType = ParamType::kInt;
+	//param->optional = false;
+
+	//cmd.params = param;
+
+	REL::safe_write((uintptr_t)original, &cmd, sizeof(cmd));
+}
+
 void ObScript::Init()
 {
-	Command* func = Command::LocateScriptCommand("AdvancePCLevel");
-	if (func) {
-		AdvancePCLevel_Commit(func);
+	Command* func1 = Command::LocateScriptCommand("AdvancePCLevel");
+	if (func1) {
+		AdvancePCLevel_Commit(func1);
+	}
+
+	Command* func2 = Command::LocateScriptCommand("GetXPForNextLevel");
+	if (func2) {
+		GetXPForNextLevel_Commit(func2);
 	}
 }
--- src/Console.h
+++ src/Console.h
@@ -8,7 +8,10 @@ namespace ObScript
 	using ParamType = RE::SCRIPT_PARAM_TYPE;
 
 	bool AdvancePCLevel_Execute(const ScriptParam* paramInfo, ScriptData* scriptData, RE::TESObjectREFR* thisObj, RE::TESObjectREFR* containingObj, RE::Script* scriptObj, RE::ScriptLocals* locals, double& result, std::uint32_t& opcodeOffsetPtr);
-	void AdvancePCLevel_Commit(Command* original);
+	bool GetXPForNextLevel_Execute(const ScriptParam* paramInfo, ScriptData* scriptData, RE::TESObjectREFR* thisObj, RE::TESObjectREFR* containingObj, RE::Script* scriptObj, RE::ScriptLocals* locals, double& result, std::uint32_t& opcodeOffsetPtr);
+	
+	void AdvancePCLevel_Commit(Command* original);	
+	void GetXPForNextLevel_Commit(Command* original);
 
 	void Init();
 }
